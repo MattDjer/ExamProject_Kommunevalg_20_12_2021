79b4f2da5d219cf7af51e5a48af3e8c52891d077
Matthias <mdjer@hotmail.com>
2021-12-21 06:11:21 +0100

    search function fixed

--- src/main/resources/static/js/footer.js
+++ src/main/resources/static/js/footer.js
@@ -9,28 +9,11 @@ document.write("  <!-- Footer -->\n" +
     "                    <div class=\"col-md-3 col-lg-3 col-xl-3 mx-auto mt-3\">\n" +
     "                        <h6 class=\"text-uppercase mb-4 font-weight-bold\">Om os</h6>\n" +
     "                        <p>\n" +
-    "                            Dette er et system til at systematisere valget i din kommune.\n" +
+    "                            Systematiser valget i din kommune.\n" +
     "                        </p>\n" +
     "                    </div>\n" +
     "                    <!-- About Us -->\n" +
     "\n" +
-    "                    <!-- Quick Links -->\n" +
-    "                    <div class=\"col-md-2 col-lg-2 col-xl-2 mx-auto mt-3\">\n" +
-    "                        <h6 class=\"text-uppercase mb-4 font-weight-bold\">Quick Links</h6>\n" +
-    "                        <p>\n" +
-    "                            <a class=\"text-white\" th:href=\"@{/createUser}\">Opret bruger</a>\n" +
-    "                        </p>\n" +
-    "                        <p>\n" +
-    "                            <a class=\"text-white\" th:href=\"@{/create-tour}\">Opret kandidat</a>\n" +
-    "                        </p>\n" +
-    "                    </div>\n" +
-    "                    <!-- Quick Links -->\n" +
-    "\n" +
-    "                    <!-- Grid column -->\n" +
-    "                    <hr class=\"w-100 clearfix d-md-none\" />\n" +
-    "                    <!-- Grid column -->\n" +
-    "\n" +
-    "                    <hr class=\"w-100 clearfix d-md-none\" />\n" +
     "\n" +
     "                    <!-- Contact -->\n" +
     "                    <div class=\"col-md-4 col-lg-3 col-xl-3 mx-auto mt-3\">\n" +
